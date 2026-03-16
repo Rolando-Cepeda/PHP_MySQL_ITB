0f704f5397c5e978ac947249604a5ab91a0b741e
Rolando_cf <rolandodj83@gmail.com>
2026-03-17 00:24:55 +0100

    Estructuras condicionales

--- condicionales.php
+++ condicionales.php
@@ -17,11 +17,81 @@ En PHP8 existen varias estructuras condicionales:
 
 
 // Realiza un programa para verificar si una persona es mayor o menor de edad.
-$edad = 7;
+$edad = 45;
 if ($edad >= 18) {
     echo "Eres mayor de edad.";
 }
 else {
     echo "Eres menor de edad";
 }
+echo "<br>";
+// Verificar si 2 números son iguales o cuál es el mayor.
+$n1 = 7;
+$n2 = 15;
+
+if ($n1 == $n2) {
+    echo "Ambos números son iguales.";
+}
+else if ($n1 > $n2) {
+    echo $n1 . ' Es mayor';
+}
+else {
+    echo $n2 . ' Es mayor';
+}
+
+echo "<br>";
+// Indicar si 3 números son iguales o cuál es el mayor.
+$num1 = 27;
+$num2 = 115;
+$num3= 18;
+if ($num1 == $num2 && $num2 == $num3) {
+    echo "Los tres números son iguales";
+}
+else if ($num1 > $num2 && $num1 > $num3) {
+    echo $num1 . " es mayor";
+}
+else if ($num2 > $num3) {
+     echo $num2 . " es mayor";
+}
+else {
+     echo $num3 . " es mayor";
+}
+
+echo "<br>";
+// switch() -> Evalúa una variable contra múltiples opciones.
+// 1 = Lunes    2 = Martes ... 7 = domingo 
+$dia = 12;
+switch($dia) {
+    case 1:
+        echo "Lunes";
+        break;
+    case 2:
+        echo "Martes";
+         break;
+    case 3:
+        echo "Miércoles";
+         break;
+    case 4:
+        echo "Jueves";
+         break;
+    case 5;
+        echo "Viernes";
+         break;
+    case 6:
+        echo "Sábado";
+         break;
+    case 7:
+        echo "Domingo";
+         break;
+    default:
+        echo "No existe el día";
+}
+/* 
+        --- CALCULADORA ---
+        1. Sumar
+        2. Restar
+        3. Nultiplicar
+        4. Dividir
+        Elija una opción: 3
+*/
 ?>
